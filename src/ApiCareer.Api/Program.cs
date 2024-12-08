ff22967b6cbf47740b587c5541ba99e6a1427435
Lucio Brito <luciopereirabritoo@gmail.com>
2024-12-08 15:54:08 -0300

    change .net version to 8
    - Change .Net Core version to 8 because of C# Dev Kit extension does not have support.
    - Change some Linux environments to supports .Net version.
    - Create launch and task for vscode debugging.

--- src/ApiCareer.Api/Program.cs
+++ src/ApiCareer.Api/Program.cs
@@ -6,7 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(e =>
 {
@@ -28,7 +27,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
